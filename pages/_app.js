658b01de3ada952e036d2683519c3de51d1f0cf6
Ashik044 <rezaashik044@gmail.com>
2022-09-11 14:24:38 +0600

    instagram

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
-import '../styles/globals.css';
-import { store } from '../Redux/store/store';
-import { Provider } from 'react-redux';
-import {SessionProvider} from 'next-auth/react';
+import "../styles/globals.css";
+import { store } from "../Redux/store/store";
+import { Provider } from "react-redux";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps:{session,...pageProps} }) {
+import NextNProgress from "nextjs-progressbar";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+      <Provider store={store}>
+        <NextNProgress
+          color="#29D"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
+        <Component {...pageProps} />
+      </Provider>
     </SessionProvider>
   );
 }
